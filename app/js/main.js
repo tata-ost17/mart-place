4f6fc42fc20ba85f5b25a97a968d6e50a162e0dc
tata-ost17 <tata.ost17@gmail.com>
2020-04-16 09:32:41 +0300

    added product__slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,15 @@
 $(function(){
+   $('.rate-star').rateYo({
+      rating: 5,
+      starWidth: '17px',
+      readOnly: true
+   });
+
+   $('.product-slider__inner').slick({
+      dots: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+   });
 
 
 });
