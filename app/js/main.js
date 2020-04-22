e30f86b66a4335c004ac328c1dae9a69c0a71c03
tata-ost17 <tata.ost17@gmail.com>
2020-04-23 00:45:10 +0300

    added section followers

--- app/js/main.js
+++ app/js/main.js
@@ -5,12 +5,27 @@ $(function(){
       readOnly: true
    });
 
+   $('.rate-stars').rateYo({
+      rating: 4.5,
+      starWidth: '15px',
+      readOnly: true
+   });
+
    $('.product-slider__inner').slick({
       arrows: true,
       slidesToShow: 1,
       slidesToScroll: 1,
    });
+
+   $('.followers-slider__inner').slick({
+      arrows: true,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+   });
+
    
 
+
+   var mixer = mixitup('.products__inner-box');
 
 });
